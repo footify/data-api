4ec4205354d8c9ff11c7be331901fa2fe6e0e664
Maxime Guedj <maxime@guedjdev.com>
2017-05-01 17:34:19 +0000

    Fix export node version

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 
-export const authSchema = require('./lib/auth/auth.schema');
+module.exports = {
+    authSchema: require('./lib/auth/auth.schema'),
+    accessTokenModel: require('./lib/auth/access-token.model'),
+    accessTokenRepository: require('./lib/auth/access-token.repository'),
+    clientModel: require('./lib/auth/client.model'),
+    clientRepository: require('./lib/auth/client.repository'),
+    refreshTokenModel: require('./lib/auth/refresh-token.model'),
+    refreshTokenRepository: require('./lib/auth/refresh-token.repository'),
+    userModel: require('./lib/user/user.model'),
+    userRepository: require('./lib/user/user.repository'),
+    userSchema: require('./lib/user/user.schema'),
+    utils: require('./lib/utils'),
 
-export const accessTokenModel = require('./lib/auth/access-token.model.js');
-export const accessTokenRepository = require('./lib/auth/access-token.repository.js');
-
-export const clientModel = require('./lib/auth/client.model.js');
-export const clientRepository = require('./lib/auth/client.repository.js');
-
-export const refreshTokenModel = require('./lib/auth/refresh-token.model.js');
-export const refreshTokenRepository = require('./lib/auth/refresh-token.repository.js');
-
-export const userModel = require('./lib/user/user.model.js');
-export const userRepository = require('./lib/user/user.repository.js');
-export const userSchema = require('./lib/user/user.schema');
-
-export const utils = require('./lib/utils');
+};
